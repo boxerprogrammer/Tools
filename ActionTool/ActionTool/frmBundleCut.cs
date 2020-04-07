95475c97d7d6350878a912c4a4b6e5b468c67a5c
Ryuichi Kawano <r_kawano@asojuku.ac.jp>
2020-04-07 20:18:01 +0900

    一括カット準備

--- ActionTool/ActionTool/frmBundleCut.cs
+++ ActionTool/ActionTool/frmBundleCut.cs
@@ -10,11 +10,43 @@ using System.Windows.Forms;
 
 namespace ActionTool
 {
+
+	public enum DividingRule
+	{
+		dr_width_height,//幅と高さで分割
+		dr_divXY//X方向およびY方向の分割数で分割
+	};
 	public partial class frmBundleCut : Form
 	{
+		
+		private DialogResult result = new DialogResult();
+		
+
 		public frmBundleCut()
 		{
 			InitializeComponent();
+		}
+
+		public DialogResult Result { get => result; set => result = value; }
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			Result = DialogResult.OK;
+			if (radioWidthHeight.Checked)
+			{
+
+			}
+			else
+			{
+
+			}
+			this.Close();
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			result = DialogResult.Cancel;
+			this.Close();
 		}
 	}
 }
