341f5d4084355135a49a4d16069a6c3cbb813ce0
r_kawano <r_kawano@asojuku.ac.jp>
2020-10-31 20:42:48 +0900

    動かなくなってたので色々と修正

--- palettetool/palettetool/frmMain.Designer.cs
+++ palettetool/palettetool/frmMain.Designer.cs
@@ -28,46 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictPicture = new System.Windows.Forms.PictureBox();
-            this.picPalette = new System.Windows.Forms.PictureBox();
-            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
-            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictPicture)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPalette)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictPicture
-            // 
-            this.pictPicture.Location = new System.Drawing.Point(16, 15);
-            this.pictPicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictPicture.Name = "pictPicture";
-            this.pictPicture.Size = new System.Drawing.Size(383, 424);
-            this.pictPicture.TabIndex = 0;
-            this.pictPicture.TabStop = false;
-            // 
-            // picPalette
-            // 
-            this.picPalette.Location = new System.Drawing.Point(407, 15);
-            this.picPalette.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.picPalette.Name = "picPalette";
-            this.picPalette.Size = new System.Drawing.Size(133, 62);
-            this.picPalette.TabIndex = 1;
-            this.picPalette.TabStop = false;
-            // 
-            // frmMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 466);
-            this.Controls.Add(this.picPalette);
-            this.Controls.Add(this.pictPicture);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "frmMain";
-            this.Text = "パレットツール";
-            this.Load += new System.EventHandler(this.frmMain_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictPicture)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPalette)).EndInit();
-            this.ResumeLayout(false);
+			this.pictPicture = new System.Windows.Forms.PictureBox();
+			this.picPalette = new System.Windows.Forms.PictureBox();
+			this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+			this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+			this.btnLoad = new System.Windows.Forms.Button();
+			this.btnPalette = new System.Windows.Forms.Button();
+			this.picIndexed = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnSave = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictPicture)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPalette)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picIndexed)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictPicture
+			// 
+			this.pictPicture.BackColor = System.Drawing.Color.White;
+			this.pictPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictPicture.Location = new System.Drawing.Point(12, 41);
+			this.pictPicture.Name = "pictPicture";
+			this.pictPicture.Size = new System.Drawing.Size(287, 384);
+			this.pictPicture.TabIndex = 0;
+			this.pictPicture.TabStop = false;
+			// 
+			// picPalette
+			// 
+			this.picPalette.BackColor = System.Drawing.Color.White;
+			this.picPalette.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picPalette.Location = new System.Drawing.Point(329, 41);
+			this.picPalette.Name = "picPalette";
+			this.picPalette.Size = new System.Drawing.Size(320, 32);
+			this.picPalette.TabIndex = 1;
+			this.picPalette.TabStop = false;
+			// 
+			// btnLoad
+			// 
+			this.btnLoad.Location = new System.Drawing.Point(329, 78);
+			this.btnLoad.Name = "btnLoad";
+			this.btnLoad.Size = new System.Drawing.Size(75, 23);
+			this.btnLoad.TabIndex = 2;
+			this.btnLoad.Text = "読み込み";
+			this.btnLoad.UseVisualStyleBackColor = true;
+			this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+			// 
+			// btnPalette
+			// 
+			this.btnPalette.Location = new System.Drawing.Point(410, 78);
+			this.btnPalette.Name = "btnPalette";
+			this.btnPalette.Size = new System.Drawing.Size(75, 23);
+			this.btnPalette.TabIndex = 3;
+			this.btnPalette.Text = "パレット抽出";
+			this.btnPalette.UseVisualStyleBackColor = true;
+			this.btnPalette.Click += new System.EventHandler(this.btnPalette_Click);
+			// 
+			// picIndexed
+			// 
+			this.picIndexed.BackColor = System.Drawing.Color.White;
+			this.picIndexed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picIndexed.Location = new System.Drawing.Point(329, 188);
+			this.picIndexed.Name = "picIndexed";
+			this.picIndexed.Size = new System.Drawing.Size(287, 237);
+			this.picIndexed.TabIndex = 4;
+			this.picIndexed.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(325, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 22);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "パレット";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label2.Location = new System.Drawing.Point(325, 164);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(114, 22);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "インデクス化";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label3.Location = new System.Drawing.Point(12, 12);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(76, 22);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "元画像";
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(491, 78);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 8;
+			this.btnSave.Text = "保存";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(660, 438);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.picIndexed);
+			this.Controls.Add(this.btnPalette);
+			this.Controls.Add(this.btnLoad);
+			this.Controls.Add(this.picPalette);
+			this.Controls.Add(this.pictPicture);
+			this.Name = "frmMain";
+			this.Text = "パレットツール";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictPicture)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPalette)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picIndexed)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -77,6 +166,13 @@
         private System.Windows.Forms.PictureBox picPalette;
         private System.Windows.Forms.SaveFileDialog dlgSave;
         private System.Windows.Forms.OpenFileDialog dlgOpen;
+	private System.Windows.Forms.Button btnLoad;
+	private System.Windows.Forms.Button btnPalette;
+	private System.Windows.Forms.PictureBox picIndexed;
+	private System.Windows.Forms.Label label1;
+	private System.Windows.Forms.Label label2;
+	private System.Windows.Forms.Label label3;
+	private System.Windows.Forms.Button btnSave;
     }
 }
 
--- palettetool/palettetool/frmMain.cs
+++ palettetool/palettetool/frmMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,95 @@ namespace palettetool
 {
     public partial class frmMain : Form
     {
+	Bitmap _img;
+	Image _palette;
+	Bitmap _paletteBMP;
+	    Bitmap _indexedBMP;
+	    
+	Color[,] _pixelData;
+	Color[,] _indexData;
+	HashSet<Color> _paletteSet=new HashSet<Color>();
+
         public frmMain()
         {
             InitializeComponent();
         }
 
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-            //Load
-        }
+	private void btnLoad_Click(object sender, EventArgs e)
+	{
+		if(dlgOpen.ShowDialog(this)== System.Windows.Forms.DialogResult.Cancel){
+			return;
+		}
+		_img=new Bitmap(Image.FromFile(dlgOpen.FileName));
+		_indexedBMP=new Bitmap(Image.FromFile(dlgOpen.FileName));
+		pictPicture.Image = _img;
+		_pixelData = new Color[_img.Width, _img.Height];
+		_indexData = new Color[_img.Width,_img.Height];
+		for (int y = 0; y < _img.Height; y++){
+			for (int x = 0; x < _img.Width; x++){
+				_pixelData[x, y] = _img.GetPixel(x, y);
+			}
+		}
+	}
+
+	private void btnPalette_Click(object sender, EventArgs e)
+	{
+		_paletteSet.Clear();
+		for (int y = 0; y < _img.Height; y++){
+			for (int x = 0; x < _img.Width; x++){
+				_paletteSet.Add( _pixelData[x, y]);
+			}
+		}
+		int i=0;
+		foreach(Color plt in _paletteSet){
+			_paletteBMP.SetPixel(i,0,plt); 
+			i++;
+		}
+		Color[] colors= _paletteSet.ToArray();
+		for (int y = 0; y < _img.Height; y++){
+			for (int x = 0; x < _img.Width; x++){
+				int idx=Array.IndexOf(colors,_img.GetPixel(x,y));
+				_indexData[x,y]=Color.FromArgb(idx,idx,idx,idx);
+				_indexedBMP.SetPixel(x,y,_indexData[x,y]);	
+			}
+		}
+
+
+		picPalette.Image =_paletteBMP;
+		picIndexed.Image=_indexedBMP;
+		Bitmap canvas = new Bitmap(picPalette.Width, picPalette.Height);
+		//ImageオブジェクトのGraphicsオブジェクトを作成する
+		Graphics g = Graphics.FromImage(canvas);
+
+		g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+		g.DrawImage(_paletteBMP, 0, 0, picPalette.Width, picPalette.Height);
+		picPalette.Image=canvas;
+		g.Dispose();
+		//canvas.Dispose();
+	}
+
+	private void frmMain_Load(object sender, EventArgs e)
+	{
+		_paletteBMP=new Bitmap(256,1,PixelFormat.Format32bppArgb);
+		
+	}
+
+	private void label2_Click(object sender, EventArgs e)
+	{
+
+	}
+
+	private void btnSave_Click(object sender, EventArgs e)
+	{
+		if (dlgSave.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+		{
+			return;
+		}
+		string folder  = System.IO.Path.GetDirectoryName(dlgSave.FileName);
+		string filename=System.IO.Path.GetFileNameWithoutExtension(dlgOpen.FileName);
+		_indexedBMP.Save(folder+"/"+filename+"_idx.png",ImageFormat.Png);
+		_paletteBMP.Save(folder+"/"+filename+"_plt.png",ImageFormat.Png);
+
+	}
     }
 }
