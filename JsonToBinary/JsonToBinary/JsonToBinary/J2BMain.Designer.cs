69bc5074ffc3281720e2ee883780fbf99a60228e
Ryuichi Kawano <zan56011@nifty.com>
2023-10-12 18:54:42 +0900

    JSONからバイナリへ変換するツール 使用するにはnuget等でSystem.Text.JSONを使う必要がある

---
+++ JsonToBinary/JsonToBinary/JsonToBinary/J2BMain.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace JsonToBinary
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadTexturePackerJSON = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.dlgOpenJSON = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnLoadTexturePackerJSON
+            // 
+            this.btnLoadTexturePackerJSON.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnLoadTexturePackerJSON.Location = new System.Drawing.Point(83, 340);
+            this.btnLoadTexturePackerJSON.Name = "btnLoadTexturePackerJSON";
+            this.btnLoadTexturePackerJSON.Size = new System.Drawing.Size(170, 60);
+            this.btnLoadTexturePackerJSON.TabIndex = 0;
+            this.btnLoadTexturePackerJSON.Text = "TexturePackerJSONをロード";
+            this.btnLoadTexturePackerJSON.UseVisualStyleBackColor = true;
+            this.btnLoadTexturePackerJSON.Click += new System.EventHandler(this.btnLoadTexturePackerJSON_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnConvert.Location = new System.Drawing.Point(519, 340);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(170, 60);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "バイナリにコンバート";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // dlgOpenJSON
+            // 
+            this.dlgOpenJSON.FileName = "texture.json";
+            this.dlgOpenJSON.Filter = "JSONファイル|*.json";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnLoadTexturePackerJSON);
+            this.Name = "frmMain";
+            this.Text = "JsonToBinary";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadTexturePackerJSON;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.OpenFileDialog dlgOpenJSON;
+    }
+}
+
---
+++ JsonToBinary/JsonToBinary/JsonToBinary/J2BMain.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace JsonToBinary
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        struct Rect
+        {
+            public int x, y;
+            public int w, h;
+        }
+        Dictionary<string,Rect> rectTable_=new Dictionary<string, Rect>();
+
+        private System.IO.Stream stream_=null;
+        
+
+        private void btnLoadTexturePackerJSON_Click(object sender, EventArgs e)
+        {
+            if(dlgOpenJSON.ShowDialog() == DialogResult.OK)
+            {
+                stream_=dlgOpenJSON.OpenFile();
+            }
+        }
+
+        private void ParseFrameForArray(JsonElement frameElement)
+        {
+            foreach (var obj in frameElement.EnumerateObject())
+            {
+                if (obj.Name == "filename")
+                {
+                    Debug.Print(obj.Value.ToString());
+                }
+            }
+        }
+
+        private void ParseFrameForHash(JsonProperty prop)
+        {
+            Debug.Print(prop.Name);
+            Rect rc;
+            rc.x = 0;
+            rc.y = 0;
+            rc.w = 0;   
+            rc.h = 0;   
+            foreach (var obj in prop.Value.EnumerateObject())
+            {
+                if (obj.Name == "frame")
+                {
+                    foreach (var rectJson in obj.Value.EnumerateObject())
+                    {
+                        if (rectJson.Name == "x")
+                        {
+                            rc.x = rectJson.Value.GetInt32();
+                        }
+                        else if (rectJson.Name == "y")
+                        {
+                            rc.y = rectJson.Value.GetInt32();
+                        }
+                        else if (rectJson.Name == "w")
+                        {
+                            rc.w = rectJson.Value.GetInt32();
+                        }
+                        else if (rectJson.Name == "h")
+                        {
+                            rc.h = rectJson.Value.GetInt32();
+                        }
+                    }
+                    break;
+                }
+            }
+            rectTable_.Add(prop.Name, rc);  
+        }
+
+        private void ParseFrames(JsonElement framesElement)
+        {
+            //TexturePackerの出力設定がArrayの時は
+            //"filename"="ファイル名"になっている
+            if (framesElement.ValueKind == JsonValueKind.Array)
+            {
+                foreach (var elem in framesElement.EnumerateArray())
+                {
+                    ParseFrameForArray(elem);
+                }
+            }
+            //TexturePackerの出力設定がハッシュの時はファイル名＝オブジェクト名
+            else if (framesElement.ValueKind == JsonValueKind.Object)
+            {
+                foreach (var obj in framesElement.EnumerateObject())
+                {
+                    ParseFrameForHash(obj);
+                }
+            }
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (stream_ == null)
+            {
+                return;
+            }
+            
+            if (stream_.Length <= stream_.Position)
+            {
+                stream_.Seek(0,System.IO.SeekOrigin.Begin);
+            }
+            var doc = JsonDocument.Parse(stream_);
+            var root = doc.RootElement;
+            rectTable_.Clear();
+            if(root.ValueKind== JsonValueKind.Object)
+            {
+                foreach(var obj in root.EnumerateObject())
+                {
+                    if (obj.Name == "frames")
+                    {
+                        ParseFrames(obj.Value);
+                    }
+                }
+            }else if (root.ValueKind == JsonValueKind.Array)
+            {
+                Debug.Print(root.ToString());
+            }
+        }
+    }
+}
